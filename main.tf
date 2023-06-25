cfe22e0a07ee02e0519627a0fe1c65327e36a041
JohnSheff <mdt-pro@mail.ru>
2023-06-25 14:15:03 +0300

    add balance

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ terraform {
   }
 }
   provider "yandex" {
-  token     = "y0_AgAAAAABr9LfAATuwQAAAADmAKHmw-6GRt7XTJ-eKAKNE-circdNa0s"
-  cloud_id  = "b1g77s9m0a5hmn7l11fk"
-  folder_id = "b1g5dnqfrovkd4k9ebl8"
+  token     = "***"
+  cloud_id  = "***"
+  folder_id = "***"
   zone      = "ru-central1-a"
 }
